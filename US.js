4f8bb5e00c55d3ef7c5f947dc2240568325f6224
Cliff Shaffer <shaffer@vt.edu>
2013-01-11 20:13:24 -0500

    Read in JavaScript data files

---
+++ US.js
@@ -0,0 +1,8 @@
+function setCountry () {
+  var myCountry = new Object;
+  myCountry.name = "United States";
+  myCountry.initPop = 300000000;
+  myCountry.startYear = 2010;
+  myCountry.growthRate = 1.02;
+  return myCountry;
+}
