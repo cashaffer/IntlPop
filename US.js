114e0fc25c840560f891f884ca78166de803400f
Cliff Shaffer <shaffer@vt.edu>
2013-01-13 19:52:47 -0500

    Added dropdown menu support for Options button

--- US.js
+++ US.js
@@ -4,5 +4,8 @@ function setCountry () {
   myCountry.initPop = 300000000;
   myCountry.startYear = 2010;
   myCountry.growthRate = 1.02;
+  myCountry.children = 2.3;
+  myCountry.lifeExp = 75.5;
+  myCountry.netMigration = 700000;
   return myCountry;
 }
