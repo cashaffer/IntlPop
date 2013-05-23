f75c0ac59ab20a5faff8628d448f5ec09c3251c6
Nate Beatty <nate.beatty@gmail.com>
2013-05-23 10:40:12 -0400

    Removed test data files

--- Canada.js
+++
@@ -1,9 +0,0 @@
-function setCountry () {
-  var myCountry = new Object;
-  myCountry.name = "Canada";
-  myCountry.startYear = 2010;
-  myCountry.netMigration = 100;
-  myCountry.malePop = [3600, 3000, 2500, 1550, 1000, 500, 100];
-  myCountry.femalePop = [3550,2750, 2300, 1500, 1050, 500, 200];
-  return myCountry;
-}
--- US.js
+++
@@ -1,9 +0,0 @@
-function setCountry () {
-  var myCountry = new Object;
-  myCountry.name = "United States";
-  myCountry.startYear = 2010;
-  myCountry.netMigration = 4955;
-  myCountry.malePop = [35000, 30000, 20000, 18000, 12000, 8000, 4000, 2000, 1000];
-  myCountry.femalePop = [35000, 29000, 21000, 19000, 13000, 10000, 5000, 3000, 2000];
-  return myCountry;
-}
